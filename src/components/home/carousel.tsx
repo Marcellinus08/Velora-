104a76507e4ce481619d5bc3d3ee46fb79188b57
abdullmalikrahman <abdullmalikrahman@gmail.com>
2025-11-25 02:24:45 +0700

    Update carousel.tsx

--- src/components/home/carousel.tsx
+++ src/components/home/carousel.tsx
@@ -10,7 +10,7 @@ const defaultSlide: SlideItem[] = [
       type: "static" as const,
       img: "https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80", 
       title: "Promote Your Content Here!", 
-      desc: "Create your campaign and reach thousands of viewers. Start advertising your videos today for just $1.", 
+      desc: "Create your campaign and reach thousands of viewers. Start advertising your videos today for just $5.", 
       cta: "Create Ad Campaign",
       href: "/ads"
     }
